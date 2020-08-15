03a2ffb5d285bd275e1739720065b2c322194ff8
Aniket Dash <dash.aniket05@gmail.com>
2020-08-15 16:54:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Container } from 'reactstrap'
+import Map from './components/Map';
 import logo from './logo.svg';
+
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header';
+require('dotenv').config();
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={ 
+      applicationName: 'TraficMaps'
+    }
+  }
+  
+
+  render(){
+    return (
+      <div className="App">
+
+       <Header appName={this.state.applicationName}/>
+
+       <Container>
+         <Map />
+       </Container>
+
+      <div id="map"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
